0565130d031e8c0932aaf0ea27c64161f6bff2b2
kremsnita420 <duranovic.safet@gmail.com>
2021-12-11 19:59:25 +0100

    contact

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -3,11 +3,15 @@ import Header from '../header/Header'
 import ScrollToButton from './ScrollTopButton'
 
 export default function Layout({ title, description, children }) {
+	const previewImage = '/images/preview.JPG'
 	return (
 		<>
 			<Head>
 				<title>{title ? `${title} - My Portfolio` : 'My Portfolio'}</title>
 				{description && <meta name='description' content={description} />}
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<meta charSet='utf-8' />
+				<meta property='og:image' content={previewImage} key='ogimage' />
 			</Head>
 			<Header />
 			<main
