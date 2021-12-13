a5d8cfbc7f722b48d02bfd974a97267e16faebf6
kremsnita420 <duranovic.safet@gmail.com>
2021-12-13 18:47:36 +0100

    footer

--- pages/project/[projectId].jsx
+++ pages/project/[projectId].jsx
@@ -55,80 +55,35 @@ export default function ProjectPage(props) {
 				))}
 			</div>
 
-			<div className='flex flex-col items-center justify-center  lg:justify-around'>
-				{/* built with */}
-				<div className='flex flex-col lg:flex-row items-center lg:items-start justify-around w-full'>
-					<div className='flex flex-col items-start justify-start'>
-						<h2 className='text-2xl font-heading1 md:text-3xl mx-auto mb-5'>
-							Built with{' '}
-						</h2>
-						<div className='flex md:flex-col items-center justify-center'>
-							<ul className='flex flex-wrap mb-10 items-start justify-start'>
-								{filteredProject.builtWith.map((tech, i) => (
-									<li
-										className='flex items-center justify-center cursor-pointer'
-										key={i}>
-										<svg
-											xmlns='http://www.w3.org/2000/svg'
-											className='h-6 w-6 mr-1'
-											fill='none'
-											viewBox='0 0 24 24'
-											stroke='currentColor'>
-											<path
-												strokeLinecap='round'
-												strokeLinejoin='round'
-												strokeWidth='2'
-												d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z'
-											/>
-										</svg>
-										<a
-											href={tech.link}
-											target='_blank'
-											rel='noopener'
-											className='underline  mr-5'>
-											{tech.name}
-										</a>
-									</li>
-								))}
-							</ul>
-						</div>
-					</div>
+			<div className='flex flex-col items-center justify-center  lg:justify-around mb-10'>
+				{/* repository and live website */}
+				<div className='flex flex-col items-start'>
+					<h2 className='text-2xl w-full font-heading1 md:text-3xl my-10 text-center'>
+						Github & Website{' '}
+					</h2>
 
-					{/* repository and live website */}
-					<div className='flex flex-col items-start'>
-						<h2 className='text-2xl w-full font-heading1 md:text-3xl mb-5 text-center'>
-							Github & Website{' '}
-						</h2>
-
-						<div className='flex items-center justify-center w-full'>
-							<button className='bg-gray-800 text-gray-200 mr-5 dark:bg-gray-200 dark:text-gray-800 px-5 my-10 sm:my-0 py-2 min-w-[6.5rem] rounded-md'>
-								<a
-									href={filteredProject.gitLink}
-									target='_blank'
-									rel='noopener'>
-									Github
-								</a>
-							</button>
-							<button className='bg-gray-800 text-gray-200 ml-5 dark:bg-gray-200 dark:text-gray-800 px-5 py-2 min-w-[6.5rem] rounded-md'>
-								<a
-									href={filteredProject.webLink}
-									target='_blank'
-									rel='noopener'>
-									Website
-								</a>
-							</button>
-						</div>
+					<div className='flex items-center justify-center w-full mb-10'>
+						<button className='bg-gray-800 text-gray-200 mr-5 dark:bg-gray-200 dark:text-gray-800 px-5 my-10 sm:my-0 py-2 min-w-[6.5rem] rounded-md'>
+							<a href={filteredProject.gitLink} target='_blank' rel='noopener'>
+								Github
+							</a>
+						</button>
+						<button className='bg-gray-800 text-gray-200 ml-5 dark:bg-gray-200 dark:text-gray-800 px-5 py-2 min-w-[6.5rem] rounded-md'>
+							<a href={filteredProject.webLink} target='_blank' rel='noopener'>
+								Website
+							</a>
+						</button>
 					</div>
 				</div>
 
 				{/* project description */}
 				<div className='flex-col md:flex-row w-full items-center text-center justify-center mx-auto mt-10'>
-					<h2 className='text-2xl font-heading1 md:text-3xl my-5'>
+					<h2 className='text-2xl font-heading1 md:text-3xl mb-10'>
 						Description{' '}
 					</h2>
 					<p className='text-lg md:text-xl'>{filteredProject.description}</p>
 
-					<h2 className='text-2xl font-heading1 md:text-3xl mb-5 mt-10'>
+					<h2 className='text-2xl font-heading1 md:text-3xl mt-20 mb-10'>
 						Technologies{' '}
 					</h2>
 					<div>
