e683ae048200a84563076bca0496fadce3b18766
kremsnita420 <duranovic.safet@gmail.com>
2021-12-30 11:39:03 +0100

    a

--- components/Projects/ProjectCard.jsx
+++ components/Projects/ProjectCard.jsx
@@ -15,16 +15,14 @@ export default function ProjectCard({ id, image, title }) {
 				<div className='imgbox'>
 					<NextImage
 						className='object-cover'
-						width={200}
-						height={200}
+						width={100}
+						height={100}
 						layout='responsive'
 						placeholder='blur'
 						blurDataURL='https://images.unsplash.com/photo-1613387275674-cb92af1c29d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80'
 						src={image}
 						alt='thumbnail'
-						priority='high'
-						rel='preload'
-						as='image'
+						s
 					/>
 					<p className='text-xl font-semibold font-heading1 text-gray-800 dark:text-gray-200 text-center pt-5'>
 						{title}
